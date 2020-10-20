76884cd1ff32806b1af81637aef8774f414450d5
Dmitry Rak <dmitryrak11@gmail.com>
2020-10-20 21:36:27 +0300

    Fixed another issue with getEnv

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,11 +3,12 @@ package tests;
 import org.testng.annotations.Test;
 import tests.another.Retry;
 
-public class LoginTest extends BaseTest{
+public class LoginTest extends BaseTest {
 
     @Test(description = "Fill 'Login page' fields with correct data", retryAnalyzer = Retry.class)
     public void correctLogin(){
+        System.out.println(System.getenv("username"));
         loginSteps
-                .login(System.getProperty("username"),System.getenv("password"));
+                .login(System.getenv("username"),System.getenv("password"));
     }
 }
