bd072217c4bcb5ce1e9bac5c93563e8c6be2fd47
Nikolai-Shims <Nikolai.Shims@gmail.com>
2020-10-29 02:28:38 +0300

    headless false

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -33,7 +33,7 @@ public class BaseTest {
         Configuration.browser = "chrome";
         Configuration.timeout = 6000;
         Configuration.clickViaJs = false;
-        Configuration.headless = true;
+        Configuration.headless = false;
         Configuration.startMaximized = true;
     }
 
