029986546a90197c13a07a0cdb2eb137537ef138
Nikolai-Shims <Nikolai.Shims@gmail.com>
2020-10-29 02:22:57 +0300

    headless true

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -31,9 +31,9 @@ public class BaseTest {
         caseSteps = new CaseSteps();
         testPlanSteps = new TestPlanSteps();
         Configuration.browser = "chrome";
-        Configuration.timeout = 3000;
+        Configuration.timeout = 6000;
         Configuration.clickViaJs = false;
-        Configuration.headless = false;
+        Configuration.headless = true;
         Configuration.startMaximized = true;
     }
 
